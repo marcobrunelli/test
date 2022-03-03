d0225b3eafa3693cb2687932397c9586f97b4da6
marcobrunelli <59649029+marcobrunelli@users.noreply.github.com>
2022-03-03 15:23:09 +0100

    Add files via upload

---
+++ src/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from './../layouts/Header.js';
+
+const NotFound = () => (
+<div id="main-container">
+   <Header/>
+   <div><br/><br/></div>
+   <h1 class="text-center">404 - Pagina non trovata!</h1>
+</div>
+ );
+
+export default NotFound;
